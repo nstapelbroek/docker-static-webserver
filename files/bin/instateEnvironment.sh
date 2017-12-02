1a615de5a78156d574081e5108e1edf36a34d06a
Nico Stapelbroek <nstapelbroek@gmail.com>
2017-12-02 20:25:35 +0100

    Only match one or more grep results

--- files/bin/instateEnvironment.sh
+++ files/bin/instateEnvironment.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/with-contenv sh
-grep -o "container.env.[a-zA-Z0-9_]*" $1 | while read -r GREPRESULT ; do
+grep -o "container.env.[a-zA-Z0-9_]\+" $1 | while read -r GREPRESULT ; do
+    # Remove the container.env. from the grep result
     ENVNAME=$(echo $GREPRESULT | cut -c15-)
+
     ENVVALUE=$(printenv $ENVNAME)
     if [ -z "$ENVVALUE" ]; then
-        (>&2 echo "Could not replace $GREPRESULT due to a missing environment variable") 
-    else 
+        (>&2 echo "Could not replace $GREPRESULT due to a missing environment variable")
+    else
         echo "Replacing $GREPRESULT with $ENVVALUE in $1"
         sed -i -e "s~$GREPRESULT~$ENVVALUE~g" $1
     fi
