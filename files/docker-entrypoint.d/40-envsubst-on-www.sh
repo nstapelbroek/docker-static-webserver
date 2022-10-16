773f78cb27957b8191f12428286a55d955731530
Nico Stapelbroek <git@nstapelbroek.com>
2022-10-16 14:51:00 +0200

    only replace files with matches in the same directory

--- files/docker-entrypoint.d/40-envsubst-on-www.sh
+++ files/docker-entrypoint.d/40-envsubst-on-www.sh
@@ -1,27 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-# A stripped down copy paste of https://github.com/nginxinc/docker-nginx/blob/master/entrypoint/20-envsubst-on-templates.sh
+# A stripped down copy of https://github.com/nginxinc/docker-nginx/blob/master/entrypoint/20-envsubst-on-templates.sh
 
 set -e
 
-ME=$(basename $0)
+ME=$(basename "$0")
 
 www_envsubst() {
   local www_dir="${NGINX_ENVSUBST_WWW_DIR:-/var/www/}"
-  local template defined_envs
 
-  defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
-  
   [ -d "$www_dir" ] || return 0
   if [ ! -w "$www_dir" ]; then
     echo >&3 "$ME: ERROR: $www_dir is not writable"
     return 0
   fi
 
-  find "$www_dir" -follow -type f -print | while read -r file; do
-    tmpfile=$(mktemp)
-    cp --attributes-only --preserve "$file" "$tmpfile"
-    envsubst "$defined_envs" < "$file" > "$tmpfile" && mv "$tmpfile" "$file"
+  grep --recursive --no-messages --files-with-matches "\${" "$www_dir" | while read -r file; do
+    envsubst < "$file" > "$file.new" && cp --attributes-only --preserve "$file" "$file.new" && mv "$file.new" "$file"
   done
 }
 
